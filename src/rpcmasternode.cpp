a4e02284ecb3412aa11d0c57b90f97ee552df453
coolblock <coldstar@protonmail.com>
2018-11-22 10:35:32 -0700

    better collateral rpc message

--- src/rpcmasternode.cpp
+++ src/rpcmasternode.cpp
@@ -388,7 +388,7 @@ UniValue masternode(const UniValue& params, bool fHelp)
         }	
 
         BOOST_FOREACH(COutput& out, vPossibleCoins) {	        
-            obj.push_back(Pair("Warning", strprintf("This output will be deprecated after block %d:", BLOCKS_AFTER_5000_COLLATERAL_CHANGE)));	
+            obj.push_back(Pair("Warning", strprintf("The output in the line below will not be useable after block %d, 5000 will be needed for collateral:", BLOCKS_AFTER_5000_COLLATERAL_CHANGE)));	
             obj.push_back(Pair(out.tx->GetHash().ToString(), strprintf("%d", out.i)));
 		}
 
