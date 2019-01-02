906ece8416c0b3d0b8c5d63f9a27ede59bde38b0
coolblock <coldstar@protonmail.com>
2019-01-02 01:04:31 -0700

    --

--- src/activemasternode.cpp
+++ src/activemasternode.cpp
@@ -210,7 +210,7 @@ void CActiveMasternode::ManageStateInitial()
     }
 
     if(pwalletMain->GetBalance() < 5000*COIN) {
-        LogPrintf("CActiveMasternode::ManageStateInitial -- %s: Wallet balance is < 1000 VIVO\n", GetStateString());
+        LogPrintf("CActiveMasternode::ManageStateInitial -- %s: Wallet balance is < 5000 VIVO\n", GetStateString());
         return;
     }
 
